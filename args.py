664737c8c3d8632d8c458f8ea4ef2be350f80253
mx-qiaoxinran <83804995+mx-qiaoxinran@users.noreply.github.com>
2021-05-28 17:28:29 +0800

    Add files via upload

---
+++ args.py
@@ -0,0 +1,14 @@
+TRAIN_DATA='./MRPC/train_data.csv'
+TEST_DATA='./MRPC/test_data.csv'
+
+VOCAB_FILE='./vocab_mrpc.txt'
+MODEL_FILE='./dssm_pr.pkl'
+
+N=3
+SENTENCE_MAXLEN=90
+CHAR_SIZE=10041
+EMBEDDING_SIZE=300
+
+EPOCH=100
+BATCH_SIZE=50
+LR=0.0005
